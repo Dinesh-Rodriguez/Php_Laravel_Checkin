76b88275702e864b225afbdbb0e87ae44d2ca5a0
Dinesh-Rodriguez <antondinesh2021@gmail.com>
2024-07-13 23:33:11 +0530

    First echo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+
+Route::resource('books', BookController::class);
+
 
 /*
 |--------------------------------------------------------------------------
